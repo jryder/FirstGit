c951efb71befef2df1e8f45e4c8d950340e9bf15
jryder@my.wctc.edu <>
2013-01-23 21:00:11 -0600

    first experiment

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -10,6 +10,6 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Woasdfasfasdfrld");
     }
 }
